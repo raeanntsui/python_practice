16a2228bc9b865653d97eb853be8366355113980
Raeann <raeann.tsui@gmail.com>
2024-06-07 15:06:49 -0700

    leap year

---
+++ Day3/day3.py
@@ -0,0 +1,6 @@
+print("Welcome to the roller coaster!")
+height = int(input("What is your height (cm)?"))
+if height < 140:
+    print("Sorry, you are not tall enough for the ride.")
+else:
+    print("Proceed to go on the ride!")
