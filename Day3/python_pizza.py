7998ebec879793b3dd2371765ff87cbd48e2cea2
Raeann <raeann.tsui@gmail.com>
2024-06-07 16:45:14 -0700

    python pizza add invalid input error handler

--- Day3/python_pizza.py
+++ Day3/python_pizza.py
@@ -4,27 +4,33 @@ size = input("What size pizza would you like to order? Please type either S, M, 
 bill = 0
 
 if (size == "S" or size == "s"):
+    valid = True
     bill += 15
 elif (size == "M" or size == "m"):
+    valid = True
     bill += 20
 elif (size == "L" or size == "l"):
+    valid = True
     bill += 25
 else:
-    print("Please be sure you are typing S, M, or L only!\n")
+    valid = False
 
-topping = input("Would you like to add PEPPERONI to your pizza? Please type 'Y' for Yes or 'N' for No.\n")
-if topping == "y" or topping == "Y":
-    if (size == "s" or size == "S"):
-        bill += 2
-    elif (size == "M" or size == "m" or size == "L" or size == "l"):
-        bill += 3
+if valid:
+    topping = input("Would you like to add PEPPERONI to your pizza? Please type 'Y' for Yes or 'N' for No.\n")
+    if topping == "y" or topping == "Y":
+        if (size == "s" or size == "S"):
+            bill += 2
+        elif (size == "M" or size == "m" or size == "L" or size == "l"):
+            bill += 3
+    else:
+        bill += 0
+
+    cheese = input("Would you like to add CHEESE to your pizza? Please type 'Y' for Yes or 'N' for No.\n")
+    if (cheese == "Y" or cheese == "y"):
+        bill += 1
+    else:
+        bill += 0
+
+    print(f"Your final bill is: ${bill}.")
 else:
-    bill += 0
-
-cheese = input("Would you like to add CHEESE to your pizza? Please type 'Y' for Yes or 'N' for No.\n")
-if (cheese == "Y" or cheese == "y"):
-    bill += 1
-else:
-    bill += 0
-
-print(f"Your final bill is: ${bill}.")
+    print("Please be sure you are typing S, M, or L only! Restart.b")
