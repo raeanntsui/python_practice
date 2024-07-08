ef7f7939468ad250ae914f750343e6ed9b50efde
Raeann <raeann.tsui@gmail.com>
2024-07-08 12:33:54 -0700

    hangman notes about plan of action

--- Day7/hangman.py
+++ Day7/hangman.py
@@ -14,7 +14,33 @@ print("""
                          "Y88P"                   
 """)
 
-lives = ["♥", "♥", "♥", "♥", "♥"]
+
+"""
+Notes:
+Before guessing...
+1. Have a log of words to choose from: words_list
+    - winning_word = randomize a word from words_list
+2. Have a lives tracker (5 lives)
+3. Have a running list of the previous guesses to make sure the player does not repeat a guess: past_guesses
+4. Have a guess_tracker => replace blanks with guesses from the user
+    guess_tracker = "_ _ _ _ _"
+
+If there are lives left (lives > 0), then prompt the user for guesses!
+
+LOOP:
+if lives > 0:
+    ask the player for a guess
+    if guess is correct aka guess matches one of the letters in winning_word:
+        guess_tracker will update the underscores in place of the correct guess
+    if guess is not correct:
+        lives -= 1
+        restart the loop to ask for a guess
+
+if lives == 0:
+    game over
+
+"""
+lives = 5
 words_list = ["hello", "raeann", "yoyo", "potato", "tomato", "smiski", "sonny"]
 hangman_word = random.choice(words_list) # pick a random word from the given list of words in "words_list"
 
